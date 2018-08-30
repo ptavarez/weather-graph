e9a813f95f708e6255be811022d4a1e50b8357ac
ptavarez16 <ptavarez16@yahoo.com>
2018-08-30 19:40:43 -0400

    Connect search bar to fetchweather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'a94bb048b5f55af8611fe5c61bdced6d';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
