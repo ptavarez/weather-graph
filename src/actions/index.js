af6d3260d8e215f8d481b1e0a856eb34eb6d5a9a
ptavarez16 <ptavarez16@yahoo.com>
2018-08-29 21:31:57 -0400

    Import axios to make get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'a94bb048b5f55af8611fe5c61bdced6d';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url)
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
