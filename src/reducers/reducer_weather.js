769ef3fab3b4cc6cfc894a38220bc28427f6bacb
ptavarez16 <ptavarez16@yahoo.com>
2018-09-09 19:42:52 -0400

    Add weather list component - Create new component to render weather information - Table set up - Render city name upon search

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+    // same as: state.concat([action.payload.data])
+    return [ action.payload.data, ...state ];
+  }
   return state;
 }
