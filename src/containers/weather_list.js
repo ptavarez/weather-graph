688df6962ab74b4b525a384bee9858bc96a00d67
ptavarez16 <ptavarez16@yahoo.com>
2018-09-09 20:28:02 -0400

    Chart component - Create new component to render charts - Pass temps and color into a chart instance for temperature

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Chart from '../components/chart'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
+    const temps = cityData.list.map(weather => weather.main.temp)
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
       </tr>
     )
   }
